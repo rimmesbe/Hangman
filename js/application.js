43077c6e928055ab9a7199444ea4f4efdb3ab07a
rimmesbe <ryanimmesberger@gmail.com>
2015-10-20 17:41:01 -0400

    unbound click events when word generated/game over

--- js/application.js
+++ js/application.js
@@ -5,22 +5,20 @@ String.prototype.replaceAt = function(index, character) {
 
 var completeWord = function(str){
   for (var i = 0; i < str.length; i++){
-    if (str.charAt(i) == "_"){
-      return false;
-    }
+    if (str.charAt(i) == "_"){return false;}
   }
   return true;
-}
+};
 
 $(document).ready(function() {
   var wordArray = ["apple", "banana", "clock", "gasoline", "animal", "dancing"];
   var guessCounter = 0;
   // grabs random word from wordArray
   $('#word-generator').on('click', function(e){
+    $('#word-generator').unbind('click');
     var randomIndex = Math.floor(Math.random() * (5 - 0 + 1));
     var secretWord = wordArray[randomIndex];
     var blankWord = "";
-    console.log(secretWord);
     for (var i = 0; i < secretWord.length; i++){
       blankWord += "_ ";
     }
@@ -38,23 +36,25 @@ $(document).ready(function() {
           letterStatus = true;
         }
       }
+      $('#secret-word').text(blankWord);
+
       if(letterStatus == false){
         guessCounter += 1;
         $('img').attr('src', ("img/hangman_"+ guessCounter.toString() +".png"));
       }
 
       if(guessCounter == 6){
+        $('li').unbind('click');
         $('#hangman-container').append("<h1>GAME OVER!</h1>");
         $('#hangman-container').append("<a href='javascript:history.go(0)'>play again?</a>");
       }
 
-      $('#secret-word').text(blankWord);
       if(completeWord(blankWord)){
+        $('li').unbind('click');
         $('#hangman-container').append("<h1>WINNER!</h1>");
         $('#hangman-container').append("<a href='javascript:history.go(0)'>play again?</a>");
       }
     });
   });
-
 
 });
